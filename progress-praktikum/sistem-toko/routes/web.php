1cfc21c42e52a5b6b6dbec6aed40929963024fdf
Ikhwan <ikhwanpratamahidayat@gmail.com>
2024-10-28 12:39:27 +0700

    progress praktikum bab 8

--- progress-praktikum/sistem-toko/routes/web.php
+++ progress-praktikum/sistem-toko/routes/web.php
@@ -23,8 +23,8 @@ Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/create', [ProductController::class, 'create'])->name("product-create");
 Route::post('/product', [ProductController::class, 'store'])->name("product-store");
 Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
-Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name("product-edit");
+Route::put('/product/{id}', [ProductController::class, 'update'])->name("product-update");
 Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 
 require __DIR__.'/auth.php';
