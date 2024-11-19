9ac5d4de4c5ca0a1c543ede60e704b16a3dc1b5e
Ikhwan <ikhwanpratamahidayat@gmail.com>
2024-11-19 12:28:44 +0700

    Progress Praktikum Bab 11

--- progress-praktikum/sistem-toko/app/Http/Controllers/ProductController.php
+++ progress-praktikum/sistem-toko/app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\ProductsExport;
+use Maatwebsite\Excel\Facades\Excel;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -10,9 +12,27 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data = Product::all(); 
+        // Ambil semua produk
+        $query = Product::query();
+
+        // Cek apakah ada parameter 'search' di request
+        if ($request->has('search') && $request->search != '') {
+
+
+            // Melakukan pencarian berdasarkan nama produk atau informasi
+            $search = $request->search;
+            $query->where(function ($q) use ($search) {
+                $q->where('product_name', 'like', '%' . $search . '%');
+            });
+        }
+
+
+        // Ambil produk dengan paginasi
+        $data = $query->paginate(2);
+
+
         return view("master-data.product-master.index-product", compact('data'));
     }
 
@@ -34,8 +54,8 @@ class ProductController extends Controller
             'unit' => 'required|string|max:50',
             'type' => 'required|string|max:50',
             'information' => 'nullable|string',
-            'qty'=>'required|integer',
-            'producer'=>'required|string|max:255'
+            'qty' => 'required|integer',
+            'producer' => 'required|string|max:255'
         ]);
 
         Product::create($validasi_data);
@@ -48,7 +68,8 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view("master-data.product-master.detail-product", compact('product'));
     }
 
     /**
@@ -57,7 +78,7 @@ class ProductController extends Controller
     public function edit(string $id)
     {
         $product = Product::findOrFail($id);
-        return view('master-data.product-master.edit-product', compact('product'));
+        return view("master-data.product-master.edit-product", compact('product'));
     }
 
     /**
@@ -65,13 +86,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $request->validate([
+        $validasi_data = $request->validate([
             'product_name' => 'required|string|max:255',
-            'unit' => 'required|string|max:255',
-            'type' => 'required|string|max:255',
+            'unit' => 'required|string|max:50',
+            'type' => 'required|string|max:50',
             'information' => 'nullable|string',
-            'qty' => 'required|integer|min:1',
-            'producer' => 'required|string|max:255',
+            'qty' => 'required|integer',
+            'producer' => 'required|string|max:255'
         ]);
 
         $product = Product::findOrFail($id);
@@ -81,10 +102,10 @@ class ProductController extends Controller
             'type' => $request->type,
             'information' => $request->information,
             'qty' => $request->qty,
-            'producer' => $request->producer,
+            'producer' => $request->producer
         ]);
 
-        return redirect()->back()->with('success', 'Product update successfully!');
+        return redirect()->back()->with('success', 'Product Created Successfully');
     }
 
     /**
@@ -92,6 +113,18 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::find($id);
+        if ($product) {
+            $product->delete();
+            return redirect()->route('product')->with('success', 'Produk Berhasil Dihapus.');
+        }
+
+        return redirect()->route('product')->with('error', 'Product Tidak Berhasil Dihapus.');
     }
+
+    public function exportExcel()
+    {
+        return Excel::download(new ProductsExport, 'product.xlsx');
+    }
+
 }
--- progress-praktikum/sistem-toko/routes/web.php
+++ progress-praktikum/sistem-toko/routes/web.php
@@ -19,15 +19,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index'])->name('product');
 Route::get('/product/create', [ProductController::class, 'create'])->name("product-create");
 Route::post('/product', [ProductController::class, 'store'])->name("product-store");
-Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name("product-edit");
-Route::put('/product/{id}', [ProductController::class, 'update'])->name("product-update");
-Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+Route::get('/product/{id}', [ProductController::class, 'show'])->name("product-detail");
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product-edit');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product-update');
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product-delete');
+Route::get('/product/export/excel', [ProductController::class, 'exportExcel'])->name('product-export-excel');
 
-require __DIR__.'/auth.php';
-
+Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier');
 Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
-Route::post('supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::get('/supplier/{id}', [SupplierController::class, 'show'])->name("supplier-detail");
+Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit'])->name('supplier-edit');
+;
+Route::put('/supplier/{id}', [SupplierController::class, 'update'])->name('supplier-update');
+Route::delete('/supplier/{id}', [SupplierController::class, 'destroy'])->name('supplier-delete');
+
+require __DIR__ . '/auth.php';
